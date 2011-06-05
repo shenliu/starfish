1927a25a6a451c04bc1f2dc206b473f9e0f4eb03
shen LIU <bonjour.shen@gmail.com>
2011-06-05 11:38:31 +0800

    doc script

--- compile/make_doc.bat
+++ compile/make_doc.bat
@@ -1,1 +1,2 @@
-java -jar D:/Programme/html/jsdoc-toolkit-2.4.0/jsrun.jar D:/Programme/html/jsdoc-toolkit-2.4.0/app/run.js -a -t=D:/Programme/html/jsdoc-toolkit-2.4.0/templates/jsdoc -e=utf-8 -d=../doc/ ../src/*.js 
+set js_doc_path="J:/Html/jsdoc_toolkit-2.4.0"
+java -jar %js_doc_path%/jsrun.jar %js_doc_path%/app/run.js -a -t=%js_doc_path%/templates/jsdoc -e=utf-8 -d=../doc ../src/*.js
