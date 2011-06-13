789dd609a828339ca607c186e19a569aeff8a594
shen LIU <bonjour.shen@gmail.com>
2011-06-13 15:49:06 +0800

    accomplish toolkit table~~

--- src/toolkit/sf_toolkit_table.js
+++ src/toolkit/sf_toolkit_table.js
@@ -4,10 +4,11 @@
  * @namespace org.shen.Starfish.toolkit
  * @submodule toolkit
  * @module table
- * @requires Array window event dom
+ * @requires String Array window event dom
  */
 starfish.toolkit.table = function() {
     var web = starfish.web;
+    var currency = '￥';
 
     /**
      * 构造函数
@@ -30,10 +31,10 @@ starfish.toolkit.table = function() {
          totalid: 'totalpages',          // {String} 所有页ID (可选)
          startingrecid: 'startrecord',   // {String} 起始条目ID (可选)
          endingrecid: 'endrecord',       // {String} 结束条目ID (可选)
-         totalrecid: 'totalrecords',     // {String} 所有条ID (可选)
+         totalrecid: 'totalrecords',     // {String} 所有条目ID (可选)
          hoverid: 'selectedrow',         // {String} 所选条目ID (可选)
          pageddid: 'pagedropdown',       // {String} 页数下拉列表ID (可选)
-         navid: 'tablenav',              // {String} Table Navigation ID (可选)
+         navid: 'tablenav',              // {String} 翻页按钮和页数下拉列表<div>ID (可选)
          sortcolumn: 1,                  // {int} 初始排序的列索引 (可选)
          sortdir: 1,                     // {int} 排序方向 (1 or -1)
          sum: [8],                       // {Array} 求和列索引 (可选)
@@ -58,7 +59,7 @@ starfish.toolkit.table = function() {
          * @method init
          */
         init: function() {
-            this.set();
+            this._set();
             var t = this.table, i = 0, d = 0;
             t.header = $$(t, 'tr')[0];
             t.rowsLen = t.rowz.length; // 表格的所有行数
@@ -110,14 +111,24 @@ starfish.toolkit.table = function() {
         },
 
         /**
+         * @method _set
+         */
+        _set: function() {
+            var t = $(this.id);
+            t.body = $$(t, 'tbody')[0];
+            t.rowz = t.body.rows;
+            this.table = t;
+        },
+
+        /**
          * @method reset
          */
         reset: function() {
             var t = this.table;
-            t.t = t.rowsLen;
+            t.total = t.rowsLen;
             for (var i = 0; i < t.rowsLen; i++) {
                 t.arr[i] = {};
-                t.arr[i].show = 1;
+                t.arr[i].show = true;
             }
             if (this.options.sortcolumn != undefined) {
                 // 按照初始排序的列索引 排序
@@ -133,12 +144,12 @@ starfish.toolkit.table = function() {
         },
 
         /**
-         * 
-         * @param {int}  idx  要排序的列索引
-         * @param {int}  f
-         * @param {int}  z
+         * @method sort
+         * @param {int}  idx   要排序的列索引
+         * @param {int}  f     排序方向
+         * @param {int}  size  每页显示的条目
          */
-        sort: function(idx, f, z) {
+        sort: function(idx, f, size) {
             var t = this.table;
             t.idx = idx;
             var x = t.header.cells[t.idx], i = 0, tbody = web.dom.elem('tbody');
@@ -176,38 +187,61 @@ starfish.toolkit.table = function() {
                 var r = t.rowz[t.arr[i].ord].cloneNode(true);
                 web.dom.insert(tbody, r);
             }
-            
             t.replaceChild(tbody, t.body);
-            this.set();
+
+            this._set();
             this.alt();
             if (this.options.paginate) {
-                this.size(z);
+                this.size(size);
             }
             this.sethover();
         },
 
+        /**
+         * 设置 鼠标在行上划过时的样式
+         *
+         * @method sethover
+         */
         sethover: function() {
             if (this.options.hoverid) {
-                for (var i = 0; i < this.table.rowsLen; i++) {
+                for (var i = 0, l = this.table.rowsLen; i < l; i++) {
                     var r = this.table.rowz[i];
-                    r.setAttribute('onmouseover', this.name + '.hover(' + i + ',1)');
-                    r.setAttribute('onmouseout', this.name + '.hover(' + i + ',0)')
+                    web.event.addEvent(r, "mouseover", new Function(this.name + '.hover(' + i + ',true)'));
+                    web.event.addEvent(r, "mouseout", new Function(this.name + '.hover(' + i + ',false)'));
                 }
             }
         },
 
+        /**
+         * 设置鼠标在行上划过时的样式
+         *
+         * @method hover
+         * @param {int}  i  行索引
+         * @param {Boolean}  d  over or out?
+         */
+        hover: function(i, d) {
+            this.table.rowz[i].id = d ? this.options.hoverid : '';
+        },
+
+        /**
+         * 计算特殊列 求和及平均数
+         *
+         * @method calc
+         */
         calc: function() {
             if (this.options.sum || this.options.avg) {
-                var t = this.table, i = x = 0, f,r;
-                if (!$$(t, 'tfoot')[0]) {
-                    f = document.createElement('tfoot');
-                    t.appendChild(f)
+                var t = this.table, i = 0, x = 0, f, r;
+                f = $$(t, 'tfoot')[0];
+                if (!f) {
+                    f = starfish.web.dom.elem('tfoot');
+                    starfish.web.dom.insert(t, f);
                 } else {
-                    f = $$(t, 'tfoot')[0];
                     while (f.hasChildNodes()) {
-                        f.removeChild(f.firstChild)
+                        f.removeChild(f.firstChild);
                     }
                 }
+
+                // 求和
                 if (this.options.sum) {
                     r = this.newrow(f);
                     for (i; i < t.colsLen; i++) {
@@ -216,147 +250,235 @@ starfish.toolkit.table = function() {
                             var s = 0, m = t.columns[i].format || '';
                             for (x = 0; x < this.table.rowsLen; x++) {
                                 if (t.arr[x].show) {
-                                    s += parseFloat(t.rowz[x].cells[i].innerHTML.replace(/(\$|\,)/g, ''))
+                                    s += parseFloat(t.rowz[x].cells[i].innerHTML.replace(/(\$|,)/g, ''));
                                 }
                             }
                             s = decimals(s, t.columns[i].decimals ? t.columns[i].decimals : 2);
-                            s = isNaN(s) ? 'n/a' : m == '$' ? s = s.currency(t.columns[i].decimals) : s + m;
-                            r.cells[i].innerHTML = s
+                            r.cells[i].innerHTML = isNaN(s) ? 'n/a' : m == currency ? s = s.currency(t.columns[i].decimals) : s + m;
                         } else {
-                            r.cells[i].innerHTML = '&nbsp;'
+                            r.cells[i].innerHTML = '&nbsp;';
                         }
                     }
                 }
+
+                // 平均数
                 if (this.options.avg) {
                     r = this.newrow(f);
                     for (i = 0; i < t.colsLen; i++) {
-                        var j = r.cells[i];
+                        j = r.cells[i];
                         if (this.options.avg.contains(i)) {
-                            var s = c = 0, m = t.columns[i].format || '';
+                            var c = 0;
+                            s = 0, m = t.columns[i].format || '';
                             for (x = 0; x < this.table.rowsLen; x++) {
                                 if (t.arr[x].show) {
-                                    s += parseFloat(t.rowz[x].cells[i].innerHTML.replace(/(\$|\,)/g, ''));
-                                    c++
+                                    s += parseFloat(t.rowz[x].cells[i].innerHTML.replace(/(\$|,)/g, ''));
+                                    c++;
                                 }
                             }
                             s = decimals(s / c, t.columns[i].decimals ? t.columns[i].decimals : 2);
-                            s = isNaN(s) ? 'n/a' : m == '$' ? s = s.currency(t.columns[i].decimals) : s + m;
-                            j.innerHTML = s
+                            j.innerHTML = isNaN(s) ? 'n/a' : m == currency ? s = s.currency(t.columns[i].decimals) : s + m;
                         } else {
-                            j.innerHTML = '&nbsp;'
+                            j.innerHTML = '&nbsp;';
                         }
                     }
                 }
             }
         },
 
+        /**
+         * 插入一行
+         *
+         * @method newrow
+         * @param {Element}  p  在哪里插入这一行
+         * @return {Element}  新插入的行
+         */
         newrow: function(p) {
-            var r = document.createElement('tr'), i = 0;
-            p.appendChild(r);
+            var r = starfish.web.dom.elem('tr'), i = 0;
             for (i; i < this.table.colsLen; i++) {
-                r.appendChild(document.createElement('td'))
+                r.appendChild(starfish.web.dom.elem('td'));
             }
+            starfish.web.dom.insert(p, r);
             return r;
         },
 
+        /**
+         * 为行列添加样式
+         *
+         * @method alt
+         */
         alt: function() {
-            var t = this.table, i = x = 0;
+            var t = this.table, i = 0, x = 0;
             for (i; i < t.rowsLen; i++) {
-                var r = t.rowz[i];
+                var row = t.rowz[i];
                 if (t.arr[i].show) {
-                    r.className = x % 2 == 0 ? this.options.evenclass : this.options.oddclass;
-                    var cells = $$(r, 'td');
+                    // 判断本行奇偶性
+                    row.className = x % 2 == 0 ? this.options.evenclass : this.options.oddclass;
+
+                    // 为排序列的每一个td加样式
+                    var cells = $$(row, 'td');
                     for (var z = 0; z < t.colsLen; z++) {
-                        cells[z].className = t.idx == z ? x % 2 == 0 ? this.options.evenselclass : this.options.oddselclass : ''
+                        cells[z].className = t.idx == z ? // 该cell是否为排序的列中的cell?
+                                (x % 2 == 0 ? this.options.evenselclass : this.options.oddselclass) : '';
                     }
-                    x++
-                }
-                if (!t.arr[i].show) {
-                    r.style.display = 'none';
+                    x++;
+                } else {
+                    starfish.web.css(row, 'display', 'none');
                 }
             }
         },
 
+        /**
+         * @method page
+         * @param {int}  s  第几页
+         */
         page: function(s) {
-            var t = this.table, i = x = 0, l = s + parseInt(this.options.size);
+            var t = this.table, i = 0, x = 0, l = s + parseInt(this.options.size);
+            // 显示 '所有条目'
             if (this.options.totalrecid) {
-                $(this.options.totalrecid).innerHTML = t.t
+                $(this.options.totalrecid).innerHTML = t.total;
             }
+
+            // 显示 '当前页'
             if (this.options.currentid) {
-                $(this.options.currentid).innerHTML = this.g
+                $(this.options.currentid).innerHTML = this.curPage;
             }
+
+            // 显示 '起始条目' 和 '结束条目'
             if (this.options.startingrecid) {
-                var b = ((this.g - 1) * this.options.size) + 1, m = b + (this.options.size - 1);
-                m = m < t.rowsLen ? m : t.t;
-                m = m < t.t ? m : t.t;
-                $(this.options.startingrecid).innerHTML = t.t == 0 ? 0 : b;
-                $(this.options.endingrecid).innerHTML = m
+                var b = ((this.curPage - 1) * this.options.size) + 1, m = b + (this.options.size - 1);
+                m = m < t.total ? m : t.total;
+                $(this.options.startingrecid).innerHTML = t.total == 0 ? 0 : b;
+                $(this.options.endingrecid).innerHTML = m;
             }
+
+            // 隐藏不属于本页的行
             for (i; i < t.rowsLen; i++) {
                 var r = t.rowz[i];
                 if (t.arr[i].show) {
                     r.style.display = x >= s && x < l ? '' : 'none';
-                    x++
+                    x++;
                 } else {
-                    r.style.display = 'none'
+                    starfish.web.css(r, 'display', 'none');
                 }
             }
         },
 
-        move: function(d, m) {
-            this.goto(d == 1 ? (m ? this.d : this.g + 1) : (m ? 1 : this.g - 1))
-        },
-
-        goto: function(s) {
-            if (s <= this.d && s > 0) {
-                this.g = s;
-                this.page((s - 1) * this.options.size)
-            }
-        },
-
+        /**
+         * 翻页按钮和页数下拉列表
+         *
+         * @method size
+         * @param {int}  s  每页显示的条目数
+         */
         size: function(s) {
+            var w = starfish.web;
             var t = this.table;
             if (s) {
                 this.options.size = s;
             }
-            this.g = 1;
-            this.d = Math.ceil(this.table.t / this.options.size);
-            if (this.options.navid) {
-                $(this.options.navid).style.display = this.d < 2 ? 'none' : 'block';
-            }
+            this.curPage = 1; // 当前页数
+            this.totalPages = Math.ceil(this.table.total / this.options.size); // 总页数
+
             this.page(0);
-            if (this.options.totalid) {
-                $(this.options.totalid).innerHTML = t.t == 0 ? 1 : this.d;
+
+            // 如果总页数大于2 则显示翻页按钮和页数下拉列表
+            if (this.options.navid) {
+                w.css($(this.options.navid), 'display', this.totalPages < 2 ? 'none' : 'block');
             }
+
+            // 显示'所有页'
+            if (this.options.totalid) {
+                $(this.options.totalid).innerHTML = t.total == 0 ? 1 : this.totalPages;
+            }
+
+            // 显示'页数下拉列表'
             if (this.options.pageddid) {
-                var d = $(this.options.pageddid), l = this.d + 1;
-                d.setAttribute('onchange', this.name + '.goto(this.value)');
+                var d = $(this.options.pageddid), l = this.totalPages + 1;
+                w.event.addEvent(d, 'change', new Function(this.name + '.goto(this.value)'));
                 while (d.hasChildNodes()) {
-                    d.removeChild(d.firstChild)
+                    d.removeChild(d.firstChild);
                 }
-                for (var i = 1; i <= this.d; i++) {
-                    var o = document.createElement('option');
+                for (var i = 1; i < l; i++) {
+                    var o = w.dom.elem('option');
                     o.value = i;
                     o.innerHTML = i;
-                    d.appendChild(o);
+                    w.dom.insert(d, o);
+                }
+            }
+
+            // 改变 '每页显示条目数下拉列表'的显示
+            var perpage = $('perpage');
+            for (i = 0; i < perpage.options.length; i++) {
+                var cv = parseInt(perpage.options[i].value);
+                if (cv > this.options.size) {
+                    perpage.options[i == 0 ? 0 : i - 1].selected = true;
+                    break;
                 }
             }
         },
 
+        /**
+         * 显示所有行
+         *
+         * @method showall
+         */
         showall: function() {
-            this.size(this.table.t);
+            this.size(this.table.total);
         },
 
-        search: function(f) {
-            var i = x = n = 0, k = -1, q = $(f).value.toLowerCase();
-            if (this.options.colddid) {
-                k = $(this.options.colddid).value
+        /**
+         * 翻页
+         *
+         * @method move
+         * @param {int}  d  1为向后  -1为向前
+         * @param {Boolean}  m  true为到第一页或最后一页
+         */
+        move: function(d, m) {
+            this.goto(d == 1 ? (m ? this.totalPages : this.curPage + 1) : (m ? 1 : this.curPage - 1))
+        },
+
+        /**
+         * 翻页
+         *
+         * @method goto
+         * @param {int}  s  翻到第几页
+         */
+        goto: function(s) {
+            if (s > 0 && s <= this.totalPages) {
+                this.curPage = s;
+                this.page((s - 1) * this.options.size);
             }
+
+            // 改变 页数下拉列表的显示
+            if (this.options.pageddid) {
+                var d = $(this.options.pageddid);
+                for (var i = 0; i < d.options.length; i++) {
+                    if (d.options[i].value == s) {
+                        d.options[i].selected = true;
+                        break;
+                    }
+                }
+            }
+        },
+
+        /**
+         * 筛选
+         *
+         * @method search
+         * @param {String}  f  筛选输入框id
+         */
+        search: function(f) {
+            var i = 0, x = 0, n = 0, k = -1, q = $(f).value.toLowerCase(); // 筛选条件
+
+            // 筛选下拉列表
+            if (this.options.colddid) {
+                k = $(this.options.colddid).value;
+            }
+            
             var s = (k == -1) ? 0 : k, e = (k == -1) ? this.table.colsLen : parseInt(s) + 1;
             for (i; i < this.table.rowsLen; i++) {
                 var r = this.table.rowz[i], v;
-                if (q == '') {
-                    v = 1
+                if (q.trim() == '') {
+                    v = 1;
                 } else {
                     for (x = s; x < e; x++) {
                         var b = r.cells[x].innerHTML.toLowerCase();
@@ -371,56 +493,78 @@ starfish.toolkit.table = function() {
                 if (v) {
                     n++;
                 }
-                this.table.arr[i].show = v;
+                
+                this.table.arr[i].show = Boolean(v);
             }
-            this.table.t = n;
+            
+            this.table.total = n;
             if (this.options.paginate) {
                 this.size();
             }
             this.calc();
             this.alt();
-        },
-
-        hover: function(i, d) {
-            this.table.rowz[i].id = d ? this.options.hoverid : '';
-        },
-
-        set: function() {
-            var t = $(this.id);
-            t.body = $$(t, 'tbody')[0];
-            t.rowz = t.body.rows;
-            this.table = t;
         }
+
     };
 
+    /**
+     * 为数字添加隔位符 (三位一个','),并显示为货币形式
+     *
+     * @method currency
+     * @param {int}  c  小数位数
+     * @return {String}  数字的货币表现形式
+     */
     Number.prototype.currency = function(c) {
         var n = this, d = n.toFixed(c).split('.');
         d[0] = d[0].split('').reverse().join('').replace(/(\d{3})(?=\d)/g, '$1,').split('').reverse().join('');
-        return '$' + d.join('.')
+        return currency + d.join('.');
     };
 
+    /**
+     * 保留小数位数
+     *
+     * @method decimals
+     * @private
+     * @param {Number}  n  要保留小数的数字
+     * @param {int}  d  要保留几位
+     * @return {Number}  数字
+     */
     function decimals(n, d) {
         return Math.round(n * Math.pow(10, d)) / Math.pow(10, d);
     }
 
+    /**
+     * 排序方法
+     *
+     * @method cp
+     * @param {Element}  f  比较的两个元素
+     * @param {Element}  c  比较的两个元素
+     */
     function cp(f, c) {
-        var g,h;
+        var g, h;
+
+        // 字符串
         f = g = f.val.toLowerCase();
         c = h = c.val.toLowerCase();
-        var i = parseFloat(f.replace(/(\$|\,)/g, '')), n = parseFloat(c.replace(/(\$|\,)/g, ''));
+
+        // 数字
+        var i = parseFloat(f.replace(/(\$|,)/g, '')), n = parseFloat(c.replace(/(\$|,)/g, ''));
         if (!isNaN(i) && !isNaN(n)) {
-            g = i,h = n
+            g = i, h = n;
         }
+
+        // 日期
         i = Date.parse(f);
         n = Date.parse(c);
         if (!isNaN(i) && !isNaN(n)) {
-            g = i;
-            h = n
+            g = i, h = n;
         }
-        return g > h ? 1 : (g < h ? -1 : 0)
+        
+        return g > h;
     }
 
     return {
         dyTable: sorter
     }
 }();
+
