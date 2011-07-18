d470ecc9ad1ce194f9784605bb9912acabe46011
shen LIU <bonjour.shen@gmail.com>
2011-07-18 17:27:20 +0800

    some change of overlay

--- src/toolkit/sf_toolkit_overlay.js
+++ src/toolkit/sf_toolkit_overlay.js
@@ -9,17 +9,12 @@
 starfish.toolkit.overlay = {
     /**
      * @method init
-     * @param {Boolean}  clickHide   点击是否隐藏overlay
      */
-    init: function(clickHide) {
+    init: function() {
         var overlay = document.createElement("div");
         overlay.id = "overlay";
 
-        if (clickHide) {
-            // 点击div该层隐藏
-            starfish.web.event.addEvent(overlay, "click", starfish.toolkit.overlay.hide);
-        }
-        starfish.web.event.addEvent(window, "resize", starfish.toolkit.overlay.show);
+        starfish.web.event.addEvent(window, "resize", starfish.toolkit.overlay.resize);
 
         document.body.appendChild(overlay);
     },
@@ -37,13 +32,24 @@ starfish.toolkit.overlay = {
      * 显示 overlay
      *
      * @method show
+     * @param {Object}  options
+     *      options {
+     *          clickHide  // 点击是否隐藏overlay
+     *          end        // 最终透明度
+     *      }
      */
-    show: function() {
+    show: function(options) {
         var w = starfish.web;
         var overlay = $("overlay");
 
         if (!overlay) {
             starfish.toolkit.overlay.init();
+            overlay = $("overlay");
+        }
+
+        if (options && options.clickHide) {
+            // 点击div该层隐藏
+            starfish.web.event.addEvent(overlay, "click", starfish.toolkit.overlay.hide);
         }
 
         w.css(overlay, "width", w.window.docWidth() + "px");
@@ -51,9 +57,16 @@ starfish.toolkit.overlay = {
 
         w.fx.fade(overlay, {
             begin: 0,
-            end:   50,
+            end:   (options && options.end) || 100,
             step:  5
         });
+    },
+
+    resize: function() {
+        var w = starfish.web;
+        var overlay = $("overlay");
+        w.css(overlay, "width", w.window.docWidth() + "px");
+        w.css(overlay, "height", w.window.docHeight() + "px");
     }
 
 };
